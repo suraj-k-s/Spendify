9edcc4b70e96a40c5b5448660209d614ff964641
leaannjyothis <leaannjyothis@gmail.com>
2024-04-22 16:26:25 +0530

    ui

---
+++ lib/data/helpquestions.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+// Mock data for income and expense categories
+const List<Map<String, dynamic>> HelpData = [
+  // Income Categories
+  {
+    'QUESTION': 'How can I add expenses and income in the app?',
+    'ANSWER':  'To add your expenses and income , simply navigate to the Expenses section and Income section of the app and enter the amount spent along with a brief description of the purchase. You can categorize expenses and imcomes to easily identify where your money is going.'
+    
+  },
+  {
+    'QUESTION': 'How can I set budget limit in the app?',
+    'ANSWER':  'To set the budget limit, go to the "Budgets" section of the app and click on "Set Budget." Enter the budget limit for each category, such as groceries, utilities, and entertainment. The app will then help you limit your spending against these budgeted amounts.' 
+  },
+  {
+    'QUESTION': 'How can get financial suggestions and education?',
+    'ANSWER':  '' 
+  },
+  {
+    'QUESTION': 'How can I can track my expenses?',
+    'ANSWER':  'To track expenses, navigate to "ANALYSIS" section of the app and select Chart or Calendar option.'
+  },
+  {
+    'QUESTION': 'Is it possible to Customize budget categories?',
+    'ANSWER':  'Absolutely! You can customize budget categories to fit your specific needs. Simply go to the "Category" section of the app and click on the button on the right side of the page. Select the specific option to "Edit" or "Delete".'
+  },
+];
--- lib/feedback.dart
+++ lib/feedback.dart
@@ -8,13 +8,37 @@ class FeedbackPage extends StatefulWidget {
 }
 
 class _FeedbackPageState extends State<FeedbackPage> {
+  final TextEditingController _textEditingController =TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: const Center(
-        child: Text('Feedback'),
-      ),
-    );
+     body: SafeArea(
+       child: Column(
+       
+         children: [
+           Padding(
+             padding: const EdgeInsets.all(8.0),
+             child: TextFormField(
+              controller: _textEditingController,
+              keyboardType: TextInputType.multiline,
+              maxLines: null,
+              minLines: 4,
+              decoration: InputDecoration(
+                hintText: 'Enter the feed back',
+              ),
+             ),
+           ),
+           TextButton(onPressed: (){}, child:Text('Cancel') ,
+           ),
+           TextButton(onPressed: (){}, 
+           child: Text("Send Feedback"),
+           ),
+         ],
+       ),
+     ),
+     );
+     
+  
   }
 }
