dde691390654c9b204ff83aec198a8792685f069
navendunandu <navendunandu@gmail.com>
2024-05-15 22:47:55 +0530

    user service

--- lib/Components/category_sheet.dart
+++ lib/Components/category_sheet.dart
@@ -1,6 +1,6 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:spendify/service/userData.dart';
 
 class CategoryBottomSheet extends StatelessWidget {
   final String type;
@@ -10,8 +10,6 @@ class CategoryBottomSheet extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final user = FirebaseAuth.instance.currentUser;
-    final userId = user?.uid;
     return Column(
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
@@ -22,60 +20,78 @@ class CategoryBottomSheet extends StatelessWidget {
         ),
         const SizedBox(height: 10.0),
         Expanded(
-          child: StreamBuilder<QuerySnapshot>(
-            stream: FirebaseFirestore.instance
-                .collection('categories')
-                .where('type', isEqualTo: type)
-                .where('userId', isEqualTo: userId)
-                .snapshots(),
-            builder: (context, snapshot) {
-              if (!snapshot.hasData) {
-                return const Center(child: CircularProgressIndicator());
+          child: FutureBuilder(
+            future: UserDataService.getData(),
+            builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Padding(
+                  padding: EdgeInsets.all(28.0),
+                  child: Center(child: CircularProgressIndicator()),
+                );
               }
 
-              List<Map<String, dynamic>> categories = [];
-
-              for (var doc in snapshot.data!.docs) {
-                var data = doc.data() as Map<String, dynamic>;
-                final category = {
-                  'id': doc.id,
-                  'name': data['name'],
-                  'icon': data['icon'],
-                  'type': data['type'],
-                };
-                categories.add(category);
+              if (snapshot.hasError || !snapshot.hasData) {
+                return Text(
+                    'Error: ${snapshot.error}'); // Handle error if unable to fetch user ID
               }
 
-              return Column(
-                children: [
-                  Expanded(
-                    child: GridView.builder(
-                      gridDelegate:
-                          const SliverGridDelegateWithFixedCrossAxisCount(
-                        crossAxisCount: 3,
-                      ),
-                      itemCount: categories.length,
-                      itemBuilder: (context, index) {
-                        final cat = categories[index];
-                        int icon = cat['icon'];
-                        return GestureDetector(
-                          onTap: () {
-                            onCategorySelected(cat['id'],
-                                cat['name']); // Call the callback function with the selected category ID
-                            Navigator.pop(context);
-                          },
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            mainAxisSize: MainAxisSize.min,
-                            children: [_buildIcon(icon), Text(cat['name'])],
+              final String familyId = snapshot.data!;
+              return StreamBuilder<QuerySnapshot>(
+                stream: FirebaseFirestore.instance
+                    .collection('categories')
+                    .where('type', isEqualTo: type)
+                    .where('family', isEqualTo: familyId)
+                    .snapshots(),
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData) {
+                    return const Center(child: CircularProgressIndicator());
+                  }
+              
+                  List<Map<String, dynamic>> categories = [];
+              
+                  for (var doc in snapshot.data!.docs) {
+                    var data = doc.data() as Map<String, dynamic>;
+                    final category = {
+                      'id': doc.id,
+                      'name': data['name'],
+                      'icon': data['icon'],
+                      'type': data['type'],
+                    };
+                    categories.add(category);
+                  }
+              
+                  return Column(
+                    children: [
+                      Expanded(
+                        child: GridView.builder(
+                          gridDelegate:
+                              const SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 3,
                           ),
-                        );
-                      },
-                    ),
-                  ),
-                ],
+                          itemCount: categories.length,
+                          itemBuilder: (context, index) {
+                            final cat = categories[index];
+                            int icon = cat['icon'];
+                            return GestureDetector(
+                              onTap: () {
+                                onCategorySelected(cat['id'],
+                                    cat['name']); // Call the callback function with the selected category ID
+                                Navigator.pop(context);
+                              },
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                mainAxisSize: MainAxisSize.min,
+                                children: [_buildIcon(icon), Text(cat['name'])],
+                              ),
+                            );
+                          },
+                        ),
+                      ),
+                    ],
+                  );
+                },
               );
-            },
+            }
           ),
         ),
       ],
